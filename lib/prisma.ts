cb84483e285862fa83dc2f6891668b3b02d87937
jwanp <joohwan726@naver.com>
2025-05-15 16:24:13 +0900

    [fix/#14] prisma.ts 에서 prisma 타입 변경 - createPrismaClient으로 감싸서 return 타입을 명시하였다.

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,20 @@
-import { PrismaClient } from '@prisma/generated'
-import { withAccelerate } from '@prisma/extension-accelerate'
+import { PrismaClient } from '@prisma/generated';
+import { withAccelerate } from '@prisma/extension-accelerate';
 
-type PrismaClientWithAccelerate = ReturnType<typeof PrismaClient.prototype.$extends>
-
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClientWithAccelerate
+function createPrismaClient() {
+  return new PrismaClient().$extends(withAccelerate());
 }
-const prisma = globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate())
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+type Prisma = ReturnType<typeof createPrismaClient>;
 
-export default prisma
+const globalForPrisma = globalThis as unknown as {
+  prisma: Prisma;
+};
+
+const prisma = globalForPrisma.prisma || createPrismaClient();
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma;
+}
+
+export default prisma;
