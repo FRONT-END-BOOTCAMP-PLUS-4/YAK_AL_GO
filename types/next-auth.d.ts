221b44e4e1c8a83d712e2289cc40a88ef3033c06
leegeon <leegun0602@naver.com>
2025-05-20 14:50:45 +0900

    [feat] next-auth 카카오provider설정

---
+++ types/next-auth.d.ts
@@ -0,0 +1,21 @@
+import type { DefaultSession } from 'next-auth'
+
+declare module 'next-auth' {
+  interface User {
+    isActive?: boolean
+    // 여기에 필요한 추가 사용자 속성 정의
+  }
+
+  interface Session {
+    user: {
+      id: string
+      isActive?: boolean
+      // 여기에 필요한 추가 사용자 속성 정의
+    } & DefaultSession['user']
+  }
+
+  interface JWT {
+    isActive?: boolean
+    // 여기에 필요한 추가 토큰 속성 정의
+  }
+}
