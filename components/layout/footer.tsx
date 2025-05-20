c8c47c821fcebd59a8ea875a9613b4a5cfecfd81
hjKim1597 <hojinkim971597@gmail.com>
2025-05-20 16:34:27 +0900

    [chore/#35] header,footer 수정 및 아이콘 추가

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,11 +1,12 @@
-import Link from "next/link"
-
+import Link from "next/link";
+import Image from "next/image";
 export default function Footer() {
   return (
     <footer className="w-full border-t bg-background">
       <div className="container flex flex-col items-center justify-between gap-4 py-6 md:flex-row">
         <div className="flex flex-col items-center gap-2 md:items-start">
           <Link href="/" className="flex items-center gap-2">
+            <Image src="/icon.png" alt="약알고" width={20} height={20} />
             <span className="font-bold text-lg text-primary">약알고</span>
           </Link>
           <p className="text-center text-sm text-muted-foreground md:text-left">
@@ -13,17 +14,37 @@ export default function Footer() {
           </p>
         </div>
         <nav className="flex gap-4 sm:gap-6">
-          <Link href="/terms" className="text-sm font-medium text-muted-foreground hover:underline">
+          {/* <Link
+            href="/terms"
+            className="text-sm font-medium text-muted-foreground hover:underline"
+          >
             이용약관
           </Link>
-          <Link href="/privacy" className="text-sm font-medium text-muted-foreground hover:underline">
+          <Link
+            href="/privacy"
+            className="text-sm font-medium text-muted-foreground hover:underline"
+          >
             개인정보처리방침
-          </Link>
-          <Link href="/contact" className="text-sm font-medium text-muted-foreground hover:underline">
-            고객센터
+          </Link> */}
+          <Link
+            href="https://github.com/FRONT-END-BOOTCAMP-PLUS-4/YAK_AL_GO"
+            className="text-sm font-medium text-muted-foreground hover:underline"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              x="0px"
+              y="0px"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              style={{ fill: "#81ded4" }}
+            >
+              <path d="M10.9,2.1c-4.6,0.5-8.3,4.2-8.8,8.7c-0.5,4.7,2.2,8.9,6.3,10.5C8.7,21.4,9,21.2,9,20.8v-1.6c0,0-0.4,0.1-0.9,0.1 c-1.4,0-2-1.2-2.1-1.9c-0.1-0.4-0.3-0.7-0.6-1C5.1,16.3,5,16.3,5,16.2C5,16,5.3,16,5.4,16c0.6,0,1.1,0.7,1.3,1c0.5,0.8,1.1,1,1.4,1 c0.4,0,0.7-0.1,0.9-0.2c0.1-0.7,0.4-1.4,1-1.8c-2.3-0.5-4-1.8-4-4c0-1.1,0.5-2.2,1.2-3C7.1,8.8,7,8.3,7,7.6C7,7.2,7,6.6,7.3,6 c0,0,1.4,0,2.8,1.3C10.6,7.1,11.3,7,12,7s1.4,0.1,2,0.3C15.3,6,16.8,6,16.8,6C17,6.6,17,7.2,17,7.6c0,0.8-0.1,1.2-0.2,1.4 c0.7,0.8,1.2,1.8,1.2,3c0,2.2-1.7,3.5-4,4c0.6,0.5,1,1.4,1,2.3v2.6c0,0.3,0.3,0.6,0.7,0.5c3.7-1.5,6.3-5.1,6.3-9.3 C22,6.1,16.9,1.4,10.9,2.1z"></path>
+            </svg>
+            GitHub
           </Link>
         </nav>
       </div>
     </footer>
-  )
+  );
 }
