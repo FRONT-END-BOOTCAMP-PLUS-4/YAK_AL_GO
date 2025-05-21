bda977398bf2bc0c2d63e377d51bfdccf519ad9e
leegeon <leegun0602@naver.com>
2025-05-21 16:39:10 +0900

    [feat/38] 세션provider연결, usestate트러블 슈팅

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,14 @@
-import type React from 'react';
-import { Inter } from 'next/font/google';
-import './globals.css';
-import Header from '@/components/layout/header';
-import Footer from '@/components/layout/footer';
-import { ThemeProvider } from '@/components/theme-provider';
-import ReactQueryProviders from '@/components/query-provider';
-const inter = Inter({ subsets: ['latin'] });
+
+"use client";
+import type React from "react";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/layout/header";
+import Footer from "@/components/layout/footer";
+import { ThemeProvider } from "@/components/theme-provider";
+import { SessionProvider } from "next-auth/react";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
@@ -18,20 +21,24 @@ export default function RootLayout({
         <title>약알고</title>
       </head>
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
-          <ReactQueryProviders>
-            <div className="flex min-h-screen flex-col">
-              <Header />
-              <main className="flex-1">{children}</main>
-              <Footer />
-            </div>
-          </ReactQueryProviders>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="light"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <div className="flex min-h-screen flex-col">
+            <Header />
+            <main className="flex-1">
+              <SessionProvider>
+                {children}
+              </SessionProvider>
+            </main>
+            <Footer />
+          </div>
         </ThemeProvider>
       </body>
     </html>
   );
 }
 
-export const metadata = {
-  generator: 'v0.dev',
-};
