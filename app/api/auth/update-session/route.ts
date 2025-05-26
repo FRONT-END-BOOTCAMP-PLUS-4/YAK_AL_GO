93753bc34d39abbfe5ae8cb7314bb2470c1bf40a
leegeon <leegun0602@naver.com>
2025-05-26 17:54:41 +0900

    [feat/#38] token api 2차 수정

--- app/api/auth/update-session/route.ts
+++ app/api/auth/update-session/route.ts
@@ -4,12 +4,13 @@ import { NextResponse } from 'next/server';
 // token 업데이트를 위한 API
 export async function POST(req: Request) {
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
-  const body = await req.json(); // 폼들어있음
+  const body = await req.json(); // 클라이언트에서 전달된 폼 데이터
 
   if (!token) {
     return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
   }
-  // jwt 토큰 업데이트
+
+  // body 데이터를 토큰에 추가
   token.id = body.id;
   token.email = body.email;
   token.photo = body.photo;
@@ -18,6 +19,14 @@ export async function POST(req: Request) {
   token.member_type = body.member_type;
   token.healthConditions = body.healthConditions;
   token.hpid = body.hpid;
+
+  // profile.kakao_account 데이터를 토큰에 추가
+  const kakaoAccount = token.profile?.kakao_account; // token.profile에서 kakao_account 추출
+  if (kakaoAccount) {
+    token.kakaoEmail = kakaoAccount.email || null; // 카카오 이메일
+    token.kakaoNickname = kakaoAccount.profile?.nickname || null; // 카카오 닉네임
+    token.kakaoImage = kakaoAccount.profile?.thumbnail_image_url || null; // 카카오 프로필 이미지
+  }
 
   return NextResponse.json({ success: true });
 }
