b9f7735eca5b48f2bbcf3940f1678d33cdb8998f
jwanp <joohwan726@naver.com>
2025-05-26 14:00:55 +0900

    [feat/#46]feat: question detail page 내용 보이도록 수정 - content_html 속성 받아와서 dangerouslySetInnerHTML 설정해준다.

--- components/qna/ContentRenderer.tsx
+++ components/qna/ContentRenderer.tsx
@@ -1,13 +1,7 @@
-'use client';
-
-import { getContentHtml } from '@/lib/community/getContentText';
-
-export function ContentRenderer({ content }: { content: any }) {
-  const html = getContentHtml(content);
-
+export function ContentRenderer({ contentHtml }: { contentHtml: string }) {
   return (
     <article className="prose prose-sm dark:prose-invert max-w-none">
-      <div dangerouslySetInnerHTML={{ __html: html }} />
+      <div dangerouslySetInnerHTML={{ __html: contentHtml }} />
     </article>
   );
 }
