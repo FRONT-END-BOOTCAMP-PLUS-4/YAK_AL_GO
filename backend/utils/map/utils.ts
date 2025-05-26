42b21868fd0a1400ab810e05fe9c7c99c5d4f135
HeekunLim <fanghk555@gmail.com>
2025-05-26 17:25:48 +0900

    [feat/#33] 시간 포맷 수정

--- backend/utils/map/utils.ts
+++ backend/utils/map/utils.ts
@@ -6,7 +6,6 @@ import type { PharmacyDto } from "../../application/usecases/pharmacy/dto/Pharma
 const calculateDistanceUseCase = new CalculateDistanceUseCase()
 const checkPharmacyOpenStatusUseCase = new CheckPharmacyOpenStatusUseCase()
 
-// 4자리 숫자 형태의 시간을 "시:분" 형태로 변환하는 함수
 export const formatTimeString = (timeStr: string | null | undefined): string | null => {
   if (!timeStr) {
     return null
@@ -16,9 +15,17 @@ export const formatTimeString = (timeStr: string | null | undefined): string | n
     return timeStr
   }
 
+  // 4자리 숫자 (ex. 0900 → 09:00)
   if (timeStr.length === 4) {
     const hour = timeStr.substring(0, 2)
     const minute = timeStr.substring(2, 4)
+    return `${hour}:${minute}`
+  }
+
+  // 3자리 숫자 (ex. 900 → 09:00)
+  if (timeStr.length === 3) {
+    const hour = `0${timeStr.substring(0, 1)}`
+    const minute = timeStr.substring(1, 3)
     return `${hour}:${minute}`
   }
 
