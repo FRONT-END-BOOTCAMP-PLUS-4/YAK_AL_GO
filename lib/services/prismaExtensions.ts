e2d8f44851888dc6466726e0d50aa64eec4bc213
hjKim1597 <hojinkim971597@gmail.com>
2025-05-27 17:44:22 +0900

    [chore/#41] 불필요한 파일 제거

--- lib/services/prismaExtensions.ts
+++
@@ -1,6 +0,0 @@
-import { Prisma } from '@prisma/generated'; // 표준 경로로 수정
-
-// 더 이상 필요 없는 확장 기능은 제거하고 기본적인 확장만 유지
-export const withBatchInsert = Prisma.defineExtension((client) => {
-  return client.$extends({});
-});
