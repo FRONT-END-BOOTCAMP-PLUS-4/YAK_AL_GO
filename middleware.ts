6a9ec6d5d3f89990c6d9e08ce646fe12fac4e87f
jwanp <joohwan726@naver.com>
2025-05-30 14:28:09 +0900

    [feat/#73] feat: member 하위 페이지 캐싱 방지 기능 추가

--- middleware.ts
+++ middleware.ts
@@ -23,6 +23,14 @@ export async function middleware(req: NextRequest) {
       signupUrl.searchParams.set('callbackUrl', req.nextUrl.pathname);
       return NextResponse.redirect(signupUrl);
     }
+
+    // member 페이지에 캐시 방지 헤더 추가
+    // 다른 페이지로 갔다가 뒤로가기 누르면 로그인 페이지로 이동하는 것을 방지하기 위함
+    const response = NextResponse.next();
+    response.headers.set('Cache-Control', 'no-cache, no-store, max-age=0, must-revalidate');
+    response.headers.set('Pragma', 'no-cache');
+    response.headers.set('Expires', '0');
+    return response;
   }
 
   return NextResponse.next();
