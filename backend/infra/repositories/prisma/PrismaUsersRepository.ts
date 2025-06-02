1d0d645809b86bafad02409e6995940970101875
leegeon <leegun0602@naver.com>
2025-06-02 15:10:29 +0900

    [feat/#94] 회원가입(약국ID)db약국id인지 검증, 오류 처리

--- backend/infra/repositories/prisma/PrismaUsersRepository.ts
+++ backend/infra/repositories/prisma/PrismaUsersRepository.ts
@@ -3,120 +3,120 @@ import { UsersRepository } from '../../../domain/repositories/UsersRepository';
 import { User } from '../../../domain/entities/UsersEntity';
 
 export class PrismaUsersRepository implements UsersRepository {
-    async createUser(user: User): Promise<User> {
-        try {
-            const userData = await prisma.users.create({
-                data: {
-                    name: user.name,
-                    email: user.email,
-                    photo: user.photo,
-                    birthyear: user.birthyear,
-                    gender : user.gender,
-                    member_type: user.member_type,
-                    created_at: user.created_at,
-                    deleted_at: user.deleted_at,
-                    hpid: user.hpid
-                }
-            });
+  async createUser(user: User): Promise<User> {
+    try {
+      const userData = await prisma.users.create({
+        data: {
+          name: user.name,
+          email: user.email,
+          photo: user.photo,
+          birthyear: user.birthyear,
+          gender: user.gender,
+          member_type: user.member_type,
+          created_at: user.created_at,
+          deleted_at: user.deleted_at,
+          hpid: user.hpid,
+        },
+      });
 
-            return new User(
-                userData.id,
-                userData.name,
-                userData.email,
-                userData.photo,
-                userData.birthyear,
-                userData.gender,
-                userData.member_type,
-                userData.created_at,
-                userData.deleted_at,
-                userData.hpid
-            );
-        } catch (error: any) {
-            throw new Error(`사용자 생성 실패: ${error.message}`);
-        }
+      return new User(
+        userData.id,
+        userData.name,
+        userData.email,
+        userData.photo,
+        userData.birthyear,
+        userData.gender,
+        userData.member_type,
+        userData.created_at,
+        userData.deleted_at,
+        userData.hpid
+      );
+    } catch (error: any) {
+      throw new Error(`올바른 hpid를 대문자로 입력하세요. 다음과 같은 오류 발생 : ${error.message}`);
     }
+  }
 
-    async findById(userId: string): Promise<User | null> {
-        try {
-            const userData = await prisma.users.findUnique({
-                where: { id: userId }
-            });
+  async findById(userId: string): Promise<User | null> {
+    try {
+      const userData = await prisma.users.findUnique({
+        where: { id: userId },
+      });
 
-            if (!userData) return null;
+      if (!userData) return null;
 
-            return new User(
-                userData.id,
-                userData.name,
-                userData.email,
-                userData.photo,
-                userData.birthyear,
-                userData.gender,
-                userData.member_type,
-                userData.created_at,
-                userData.deleted_at,
-                userData.hpid
-            );
-        } catch (error: any) {
-            throw new Error(`사용자 조회 실패: ${error.message}`);
-        }
+      return new User(
+        userData.id,
+        userData.name,
+        userData.email,
+        userData.photo,
+        userData.birthyear,
+        userData.gender,
+        userData.member_type,
+        userData.created_at,
+        userData.deleted_at,
+        userData.hpid
+      );
+    } catch (error: any) {
+      throw new Error(`사용자 조회 실패: ${error.message}`);
     }
+  }
 
-    async findByEmail(email: string): Promise<User | null> {
-        try {
-            const userData = await prisma.users.findUnique({
-                where: { email }
-            });
+  async findByEmail(email: string): Promise<User | null> {
+    try {
+      const userData = await prisma.users.findUnique({
+        where: { email },
+      });
 
-            if (!userData) return null;
+      if (!userData) return null;
 
-            return new User(
-                userData.id,
-                userData.name,
-                userData.email,
-                userData.photo,
-                userData.birthyear,
-                userData.gender,
-                userData.member_type,
-                userData.created_at,
-                userData.deleted_at,
-                userData.hpid
-            );
-        } catch (error: any) {
-            throw new Error(`사용자 조회 실패: ${error.message}`);
-        }
+      return new User(
+        userData.id,
+        userData.name,
+        userData.email,
+        userData.photo,
+        userData.birthyear,
+        userData.gender,
+        userData.member_type,
+        userData.created_at,
+        userData.deleted_at,
+        userData.hpid
+      );
+    } catch (error: any) {
+      throw new Error(`사용자 조회 실패: ${error.message}`);
     }
+  }
 
-    async updateUser(id: string, user: Partial<User>): Promise<User> {
-        try {
-            const userData = await prisma.users.update({
-                where: { id },
-                data: {
-                    name: user.name,
-                    email: user.email,
-                    photo: user.photo,
-                    birthyear: user.birthyear,
-                    gender : user.gender,
-                    member_type: user.member_type,
-                    created_at: user.created_at,
-                    deleted_at: user.deleted_at,
-                    hpid: user.hpid
-                }
-            });
+  async updateUser(id: string, user: Partial<User>): Promise<User> {
+    try {
+      const userData = await prisma.users.update({
+        where: { id },
+        data: {
+          name: user.name,
+          email: user.email,
+          photo: user.photo,
+          birthyear: user.birthyear,
+          gender: user.gender,
+          member_type: user.member_type,
+          created_at: user.created_at,
+          deleted_at: user.deleted_at,
+          hpid: user.hpid,
+        },
+      });
 
-            return new User(
-                userData.id,
-                userData.name,
-                userData.email,
-                userData.photo,
-                userData.birthyear,
-                userData.gender,
-                userData.member_type,
-                userData.created_at,
-                userData.deleted_at,
-                userData.hpid
-            );
-        } catch (error: any) {
-            throw new Error(`사용자 수정 실패: ${error.message}`);
-        }
+      return new User(
+        userData.id,
+        userData.name,
+        userData.email,
+        userData.photo,
+        userData.birthyear,
+        userData.gender,
+        userData.member_type,
+        userData.created_at,
+        userData.deleted_at,
+        userData.hpid
+      );
+    } catch (error: any) {
+      throw new Error(`사용자 수정 실패: ${error.message}`);
     }
+  }
 }
